2fd684b1c839c87b7d720b49be7cb10e575c151d
makualiyev <maku.aliyev@gmail.com>
2024-08-07 20:38:41 +0300

    (08-string-to-integer-atoi): working solution in C (runtime beats 100%; memory beats 6.02%)

--- 08-string-to-integer-atoi/c/solution.c
+++ 08-string-to-integer-atoi/c/solution.c
@@ -27,7 +27,13 @@
     TESTCASE: s = "1337c0d3"            //  1337
     TESTCASE: s = "0-1"                 //  0
     TESTCASE: s = "words and 987"       //  0
-    TESTCASE: s = "-91283472332"        //  0 runtime error: signed integer overflow: -2147483648 * 9 cannot be represented in type 'int'
+    TESTCASE: s = "-91283472332"        //  -2147483648 runtime error: signed integer overflow: -2147483648 * 9 cannot be represented in type 'int'
+    TESTCASE: s = ""
+    TESTCASE: s = "-"
+    TESTCASE: s = "+1"                  //  1
+    TESTCASE: s = "20000000000000000000"
+    TESTCASE: s = "  0000000000012345678"
+    TESTCASE: s = "  +  413"
 
                                                 
 */
@@ -52,29 +58,60 @@ char* stripStr(char* s, int* isMinusSigned)
     // int _isMinusSigned = 0;
 
     // counters
-    int caret = 0, caretDigit = 0;
+    int caret = 0, caretDigit = 0, isCharDigit = 0, prevWasSign = 0, isLeadingZero = 0;
 
     while (1)
     {
-        int isCharDigit = 0;
+        isCharDigit = 0;
 
         if (s[caret] == '\0') {
+            if (caret == 0) {
+                digits = NULL;
+                break;
+            } 
+            if (!isDigitSeen) {
+                digits = NULL;
+                break;
+            }
+            if (isLeadingZero) {
+                digits = NULL;
+                break;
+            }
+
             if (caretDigit >= digitsInitSize) {
                 digitsInitSize = caretDigit + 1;
                 digits = (char*)realloc(digits, sizeof(char) * digitsInitSize);
                 digits[caretDigit] = '\0';
             }
             break;
+
         }
 
         for (int j = 0; j < strlen(digitsSet); j++)
         {
             if (digitsSet[j] == s[caret])
             {
+                if (isLeadingZero && s[caret] == '0') {
+                    isDigitSeen = 1;
+                    isLeadingZero = 1;
+                    isCharDigit = 1;
+                    break;
+                }
+                if (!isDigitSeen && s[caret] == '0') {
+                    isDigitSeen = 1;
+                    isLeadingZero = 1;
+                    isCharDigit = 1;
+                    if (caretDigit >= digitsInitSize) {
+                        digitsInitSize = caretDigit + 1;
+                        digits = (char*)realloc(digits, sizeof(char) * digitsInitSize);
+                    }
+                    digits[caretDigit] = s[caret];
+                    caretDigit += 1;
+                    break;
+                }
                 isDigitSeen = 1;
                 isCharDigit = 1;
-
-                // printf("caret:%d j:%d char:%c char_num:%d\n", caret, j, s[caret], (int)s[caret] - 48);
+                isLeadingZero = 0;
 
                 if (caretDigit >= digitsInitSize) {
                     digitsInitSize = caretDigit + 1;
@@ -87,10 +124,12 @@ char* stripStr(char* s, int* isMinusSigned)
 
         if (isCharDigit) {
             caret++;
+            prevWasSign = 0;
             continue;
         }
         else {
             if (isDigitSeen) {
+
                 if (caretDigit >= digitsInitSize) {
                     digitsInitSize = caretDigit + 1;
                     digits = (char*)realloc(digits, sizeof(char) * digitsInitSize);
@@ -100,77 +139,112 @@ char* stripStr(char* s, int* isMinusSigned)
             }
             else {
                 if (s[caret] == ' ') {
+                    if (prevWasSign) {
+                        digits = NULL;
+                        break;
+                    }
+                    prevWasSign = 0;
                     caret++;
                     continue;
                 }
+
                 else if (s[caret] == '-') {
-                    // isMinusSigned = 1;
+                    if (prevWasSign) {
+                        digits = NULL;
+                        break;
+                    }
+                    
+                    prevWasSign = 1;
                     *isMinusSigned = 1;
                     caret++;
                     continue;
                 }
+                
+                else if (s[caret] == '+') {
+                    if (prevWasSign) {
+                        digits = NULL;
+                        break;
+                    }
+
+                    prevWasSign = 1;
+                    caret++;
+                    continue;
+                }
+
                 else {
+
                     if (caretDigit >= digitsInitSize) {
                         digitsInitSize = caretDigit;
                         digits = (char*)realloc(digits, sizeof(char) * digitsInitSize);
                         digits[caretDigit] = '\0';
                     }
+
                     else {
                         digits[caretDigit] = '\0';
                     }
+                    
                     break;
                 }
             }
-
-
         }
     }
-
-    // for (int j = 0; j < digitsInitSize; j++) {
-    //     printf("DEBUG|j:%d - digits[j]: %c | digitsInitSize %d\n", j, digits[j], digitsInitSize);
-    // }
     return digits;
 }
 
 int intAtoi(char* s, int* isMinusSigned) {
-    int result = 0;
+    long long result = 0;
+
+    if (strlen(s) > 14)
+    {
+        // string length greater than 10 e . g. INTMAX
+        if (*isMinusSigned) {
+            return INT_MIN;
+        } else {
+            return INT_MAX;
+        }
+    }
 
     for (int i = 0; i < strlen(s); i++) {
         int digit = ((int)s[i] - 48);
-        result += digit * (int)pow(10, (strlen(s) - i - 1));
+        result += (long long)digit * (long long)pow(10, (int)(strlen(s) - i - 1));
+        printf("\tDEBUG|i:%d digit:%c digit_int:%d pow:%d\tresult:%lld\n", i, s[i], digit, (int)(strlen(s) - i - 1), result);
     }
 
     if (*isMinusSigned)
     {
-        return (result * (-1));
+        result = (result * (-1LL));
     }
 
-    return result;
+    if (result < INT_MIN) {
+        return INT_MIN;
+    }
+    else if (result > INT_MAX) {
+        return INT_MAX;
+    } else {
+        return (int)result;
+    }
+
 }
 
 
 int myAtoi(char* s) {
     int result = 0;
     int isMinusSigned = 0;
-
     char* strippedS = stripStr(s, &isMinusSigned);
+
+    if (strippedS == NULL) {
+        return 0;
+    }
+
     result = intAtoi(strippedS, &isMinusSigned);
-
     printf("Stripped string: %s\n", strippedS);
-
     return result;
 }
 
 int main(int argc, char* argv[])
 {
-    char* testCase = "-91283472332";
+    char* testCase = "  +  413";
     int result = myAtoi(testCase);
-
-
-    // TESTCASE: s = " -042"               //  -42
-    // TESTCASE: s = "1337c0d3"            //  1337
-    // TESTCASE: s = "0-1"                 //  0
-    // TESTCASE: s = "words and 987"       //  0
 
     printf("\ntestCase: %s\nresult:\t%d\n", testCase, result);
     printf("%d\n", '1');
